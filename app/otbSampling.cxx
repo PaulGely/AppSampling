55c0a32fd5ca9f070c5ae2d6bde0c3b8b25f0a00
gelyp <paul.gely@cnes.fr>
2015-04-28 10:10:54 +0200

    Ajout des copyrights

--- app/otbSampling.cxx
+++ app/otbSampling.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
+
+
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
+
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ =========================================================================*/
+
 #include "otbImage.h"
 #include "otbImageFileReader.h"
 #include "otbImageFileWriter.h"
@@ -374,7 +391,11 @@ private:
                   if(featIt->ogr().GetFieldDefnRef(c)->GetType() == OFTString )
                   {
                     dstFeature.ogr().SetField(nbComp + c, featIt->ogr().GetFieldAsString(c));
-                  }    
+                  }  
+                  else if(featIt->ogr().GetFieldDefnRef(c)->GetType() == OFTInteger )
+                  {
+                    dstFeature.ogr().SetField(nbComp + c, featIt->ogr().GetFieldAsInteger(c));
+                  }
                 }  
                 
                 //msg += " " + to_string(point[0]) + " " + to_string(point[1]);
